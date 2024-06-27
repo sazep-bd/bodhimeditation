38d72c0c62fffe0a335d9db04eca6f23ed28d2a6
Joono Chakma <73777352+joonopohr@users.noreply.github.com>
2024-06-28 09:11:35 +1000

    add disclosure ui component for the navlinks

--- src/components/molecules/nav-links.tsx
+++ src/components/molecules/nav-links.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react';
 import React from 'react';
 
 
@@ -8,6 +9,44 @@ const Navlinks = () => {
       <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
         Home
       </a>
+      <Disclosure>
+        <DisclosureButton className="py-2">Events</DisclosureButton>
+        <DisclosurePanel className="text-gray-900">
+          <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
+            Regular Events
+          </a>
+          <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
+            Special Events
+          </a>
+          <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
+            Dhamma Teaching
+          </a>
+          <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
+            Learning Buddhism
+          </a>
+          <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
+            Learning Meditation
+          </a>
+          <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
+            Fung
+          </a>
+        </DisclosurePanel>
+      </Disclosure>
+      <Disclosure>
+        <DisclosureButton className="py-2">Support us</DisclosureButton>
+        <DisclosurePanel className="text-gray-900">
+          <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
+            Dana
+          </a>
+          <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
+            Donation
+          </a>
+          <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
+            Volunteering 
+          </a>
+        </DisclosurePanel>
+      </Disclosure>
+
       <a
         href="/resources"
         className="text-sm font-semibold leading-6 text-gray-900"
