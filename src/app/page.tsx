9b1d8a9c34822de34b274a49a75efa9834e118c0
Nishan Chakma <nishan.chakma.nish@gmail.com>
2024-07-08 09:51:04 +1000

    home-page-image-card added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import ImageWithLink from "@/components/atoms/BodhiLogo";
 import Header from "@/components/organisms/header";
 import Image from "next/image";
+import Card from "@/components/atoms/Card";
 
 export default function Home() {
   return (
@@ -8,6 +9,17 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <Header />
       </div>
+
+      <Card className="flex flex-col gap-2 bg-white text-black max-w-xs p-4">
+        <div className="bg-[url('/images/Learning_Bhuddhism_2.jpg')] h-64 bg-center justify-center"></div>
+        {/* <img src="\images\Learning_Bhuddhism_2.jpg" alt="" /> */}
+        <div className=" text-4xl text-center">Dhamma Teaching</div>
+        <div className="bg-light-grey w-fit p-2 rounded">
+          Please join our weekly meditation session to learn about Buddhism and Meditation.
+          Add more text here...
+        </div>
+      </Card>
+      
     </main>
   );
 }
