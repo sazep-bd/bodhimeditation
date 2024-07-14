bbc1ec1dc458690f2453f8b8cf07524a47c54d2d
Devjoy Chakma <chakmadevjoy@gmail.com>
2024-07-14 22:56:28 +1000

    fixed-styling

--- src/components/atoms/emphasize-paragraph.tsx
+++ src/components/atoms/emphasize-paragraph.tsx
@@ -5,7 +5,7 @@ export interface EmphasizeParagraphProps {
 
 function EmphasizeParagraph(props: EmphasizeParagraphProps){
     return (
-     <p className="text-stone-500 font-normal text-2xl">{props.content}</p>
+     <p className="gittext-center text-stone-500 font-normal text-4xl">{props.content}</p>
            
     );
 }
