168bf67f2be4958983e8e4d1e80f268fa17d0b97
Devjoy Chakma <chakmadevjoy@gmail.com>
2024-07-14 23:00:22 +1000

    add-function-contact-card

--- src/components/molecules/contact-us.tsx
+++ src/components/molecules/contact-us.tsx
@@ -1,3 +1,20 @@
 import Button from "../atoms/button";
 import EmphasizeParagraph from "../atoms/emphasize-paragraph";
 
+export interface ContactCardProps {
+    label?: string;
+    href: string;
+    content: string;
+
+}
+
+function ContactCard(props:ContactCardProps){
+    return (
+        <div>
+            <EmphasizeParagraph content={props.content}></EmphasizeParagraph>
+            <Button label={props.label} href={props.href}></Button>
+        </div>
+    )
+}
+
+export default ContactCard;
