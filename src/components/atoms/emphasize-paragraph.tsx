9a629f027eca2f6934903971c329a37f28ef4a07
Devjoy Chakma <chakmadevjoy@gmail.com>
2024-07-27 19:38:19 +1000

    fixed-small-screen-response

--- src/components/atoms/emphasize-paragraph.tsx
+++ src/components/atoms/emphasize-paragraph.tsx
@@ -5,7 +5,11 @@ export interface EmphasizeParagraphProps {
 
 function EmphasizeParagraph(props: EmphasizeParagraphProps){
     return (
-     <p className="text-center text-stone-500 font-normal text-4xl">{props.content}</p>
+     <p className="text-center max-w-full sm:max-w-3xl p-4 sm:p-6 md:p-8 lg:p-10 
+     text-base sm:text-lg md:text-2xl lg:text-3xl text-stone-500 font-normal text-4xl
+     w-40 sm:w-80 md:w-full lg:w-full max-w-full 
+     
+      ">{props.content}</p>
            
     );
 }
@@ -13,3 +17,4 @@ function EmphasizeParagraph(props: EmphasizeParagraphProps){
 
 
 export default EmphasizeParagraph;
+
