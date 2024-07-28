c9a42de8a4eceb2dcac1f62aaff4559f4fff6ce2
Joono Chakma <73777352+joonopohr@users.noreply.github.com>
2024-07-29 01:12:45 +1000

    fix styling when disclosure is clicked

--- src/components/molecules/nav-links.tsx
+++ src/components/molecules/nav-links.tsx
@@ -13,60 +13,91 @@ const Navlinks = () => {
       <a href="/" className="text-sm leading-6 text-gray-900">
         Home
       </a>
-      <Disclosure>
-        <div className="flex items-center">
-          <DisclosureButton className="py-2 text-gray-900 flex items-center">
-            Events
-            <ChevronDownIcon
-              aria-hidden="true"
-              className="h-5 w-5 text-gray-900"
-            />
-          </DisclosureButton>
-        </div>
-        <DisclosurePanel className="text-gray-900 grid grid-cols-1">
-          <a href="/" className="text-sm leading-6 text-gray-900">
-            Regular Events
-          </a>
-          <a href="/" className="text-sm leading-6 text-gray-900">
-            Special Events
-          </a>
-          <a href="/" className="text-sm leading-6 text-gray-900">
-            Dhamma Teaching
-          </a>
-          <a href="/" className="text-sm leading-6 text-gray-900">
-            Learning Buddhism
-          </a>
-          <a href="/" className="text-sm leading-6 text-gray-900">
-            Learning Meditation
-          </a>
-          <a href="/" className="text-sm leading-6 text-gray-900">
-            Fung
-          </a>
-        </DisclosurePanel>
-      </Disclosure>
+      <div className="relative">
+        <Disclosure>
+          <div className="flex items-center">
+            <DisclosureButton className="py-2 text-gray-900 flex items-center">
+              Events
+              <ChevronDownIcon
+                aria-hidden="true"
+                className="h-5 w-5 text-gray-900"
+              />
+            </DisclosureButton>
+          </div>
+          <DisclosurePanel className="absolute left-0 mt-2 w-48 bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none text-gray-900 grid grid-cols-1">
+            <a
+              href="/"
+              className="text-sm leading-6 text-gray-900 p-2 hover:bg-gray-100"
+            >
+              Regular Events
+            </a>
+            <a
+              href="/"
+              className="text-sm leading-6 text-gray-900 p-2 hover:bg-gray-100"
+            >
+              Special Events
+            </a>
+            <a
+              href="/"
+              className="text-sm leading-6 text-gray-900 p-2 hover:bg-gray-100"
+            >
+              Dhamma Teaching
+            </a>
+            <a
+              href="/"
+              className="text-sm leading-6 text-gray-900 p-2 hover:bg-gray-100"
+            >
+              Learning Buddhism
+            </a>
+            <a
+              href="/"
+              className="text-sm leading-6 text-gray-900 p-2 hover:bg-gray-100"
+            >
+              Learning Meditation
+            </a>
+            <a
+              href="/"
+              className="text-sm leading-6 text-gray-900 p-2 hover:bg-gray-100"
+            >
+              Fung
+            </a>
+          </DisclosurePanel>
+        </Disclosure>
+      </div>
 
-      <Disclosure>
-        <div className="flex items-center">
-          <DisclosureButton className="py-2 text-gray-900 flex items-center">
-            Support us
-            <ChevronDownIcon
-              aria-hidden="true"
-              className="h-5 w-5 text-gray-900"
-            />
-          </DisclosureButton>
-        </div>
-        <DisclosurePanel className="text-gray-900 grid grid-cols-1">
-          <a href="/" className="text-sm leading-6 text-gray-900">
-            Dana
-          </a>
-          <a href="/" className="text-sm leading-6 text-gray-900">
-            Donation
-          </a>
-          <a href="/" className="text-sm leading-6 text-gray-900">
-            Volunteering
-          </a>
-        </DisclosurePanel>
-      </Disclosure>
+      <div className="relative">
+        <Disclosure>
+          <div className="flex items-center">
+            <DisclosureButton className="py-2 text-gray-900 flex items-center">
+              Support us
+              <ChevronDownIcon
+                aria-hidden="true"
+                className="h-5 w-5 text-gray-900"
+              />
+            </DisclosureButton>
+          </div>
+          <DisclosurePanel className="absolute left-0 mt-2 w-48 bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none text-gray-900 grid grid-cols-1">
+            <a
+              href="/"
+              className="text-sm leading-6 text-gray-900 p-2 hover:bg-gray-100"
+            >
+              Dana
+            </a>
+            <a
+              href="/"
+              className="text-sm leading-6 text-gray-900 p-2 hover:bg-gray-100"
+            >
+              Donation
+            </a>
+            <a
+              href="/"
+              className="text-sm leading-6 text-gray-900 p-2 hover:bg-gray-100"
+            >
+              Volunteering
+            </a>
+          </DisclosurePanel>
+        </Disclosure>
+      </div>
 
       <a href="/resources" className="text-sm leading-6 text-gray-900">
         Resources
